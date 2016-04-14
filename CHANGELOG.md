866144b6e9f98cf07ec74c644bedf75a9554442c
Jim Kelly <pthread1981@gmail.com>
2016-04-14 22:35:08 +0200

    Fix issue with pypandoc in setup.py

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
 A list of versions and changes to the project:
 
+- v0.0.4 - Fix issue with pypandoc usage.
 - v0.0.3 - Fixed issue with boolean defaults, allow for type_class and UnsetExceptions.
-- v0.0.2 - Fixed issue with ConfigParser lowercasing keys
-- v0.0.1 - Initial commit to GitHub
+- v0.0.2 - Fixed issue with ConfigParser lowercasing keys.
+- v0.0.1 - Initial commit to GitHub.
--- setup.py
+++ setup.py
@@ -2,6 +2,16 @@ import os
 from distutils.core import setup
 from setuptools import find_packages
 
+
+try:
+    import pypandoc
+except ImportError:
+    class pypandoc(object):
+        @classmethod
+        def convert(self, data, type, format):
+            return data
+
+
 __version__ = 'Unset'
 with open('src/yaep/version.py') as vfp:
     vd = vfp.read().strip()
@@ -12,16 +22,21 @@ if os.path.exists('requirements.txt'):
     requirements = [r.strip() for r in open('requirements.txt')]
 
 
+readme = open('README.md').read()
+changelog = open('CHANGELOG.md').read()
+long_description = readme + '\n\n' + changelog
+
+
 config = {
     'description': 'YAEP - Yet Another Environment Package',
-    'long_description_markdown_filename': 'README.md',
+    'long_description': pypandoc.convert(long_description, 'rst', format='md'),
     'author': 'James Kelly',
     'url': 'https://github.com/jimjkelly/yaep',
     'version': __version__,
     'install_requires': requirements,
     'tests_require': ['nose'],
     'test_suite': 'nose.collector',
-    'setup_requires': ['setuptools-markdown'],
+    'setup_requires': ['pypandoc'],
     'packages': find_packages('src'),
     'package_dir': {'': 'src'},
     'scripts': [],
